08d10d211f3449fb182cdce4efa54d6ec5355095
kevinferreiradev <ferreirakevin325@gmail.com>
2022-04-03 00:01:23 -0300

    ensure LoginRouter return 400 password

--- src/presentation/routes/login-router.spec.js
+++ src/presentation/routes/login-router.spec.js
@@ -1,7 +1,7 @@
 
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -15,6 +15,17 @@ describe('Login Router', () => {
     const httpRequest = {
       body: {
         password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return if no email is provider', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com'
       }
     }
     const httpResponse = sut.route(httpRequest)
