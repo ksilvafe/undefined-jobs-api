3b550888f67caa172303d7100289422092eceb16
kevinferreiradev <ferreirakevin325@gmail.com>
2022-04-13 21:49:22 -0300

    ensure Encrypter reutns true bcript returns true

---
+++ src/utils/helpers/encrypter.spec.js
@@ -0,0 +1,12 @@
+class Encrypter {
+  async compare (password, hashedPassword) {
+    return true
+  }
+}
+describe('Encrypter', () => {
+  test('Should return bcrypt returns true', async () => {
+    const sut = new Encrypter()
+    const isValid = await sut.compare('any_password', 'hashed_password')
+    expect(isValid).toBe(true)
+  })
+})
