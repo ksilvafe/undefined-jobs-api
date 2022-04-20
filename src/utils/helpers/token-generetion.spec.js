0cf8684e480f00b64aa44a3b08abbb0815a72c20
kevinferreiradev <ferreirakevin325@gmail.com>
2022-04-20 00:21:24 -0300

    Should call JWT with correct values

--- src/utils/helpers/token-generetion.spec.js
+++ src/utils/helpers/token-generetion.spec.js
@@ -1,12 +1,16 @@
 const jwt = require('jsonwebtoken')
 class TokenGenerator {
+  constructor (secret) {
+    this.secret = secret
+  }
+
   async generate (id) {
-    return jwt.sign(id, 'secret')
+    return jwt.sign(id, this.secret)
   }
 }
 
 const makeSut = () => {
-  return new TokenGenerator()
+  return new TokenGenerator('secret')
 }
 
 describe('TokenGeneretor', () => {
@@ -21,5 +25,12 @@ describe('TokenGeneretor', () => {
     const sut = makeSut()
     const token = await sut.generate('any_id')
     expect(token).toBe(jwt.token)
+  })
+
+  test('Should call JWT with correct values', async () => {
+    const sut = makeSut()
+    await sut.generate('any_id')
+    expect(jwt.id).toBe('any_id')
+    expect(jwt.secret).toBe(sut.secret)
   })
 })
