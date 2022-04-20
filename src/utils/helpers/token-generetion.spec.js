1e9215d57fcdd4e3c4045f80fe62d2ea4261b703
kevinferreiradev <ferreirakevin325@gmail.com>
2022-04-20 00:10:52 -0300

    ensure TokenGeneration returns a token if JWT returns token

--- src/utils/helpers/token-generetion.spec.js
+++ src/utils/helpers/token-generetion.spec.js
@@ -1,13 +1,21 @@
+const jwt = require('jsonwebtoken')
 class TokenGenerator {
-  async generate () {
-    return null
+  async generate (id) {
+    return jwt.sign(id, 'secret')
   }
 }
 
 describe('TokenGeneretor', () => {
   test('Should return null if JWT returns null', async () => {
     const sut = new TokenGenerator()
+    jwt.token = null
     const token = await sut.generate('any_id')
     expect(token).toBeNull()
+  })
+
+  test('Should return token if JWT returns token', async () => {
+    const sut = new TokenGenerator()
+    const token = await sut.generate('any_id')
+    expect(token).toBe(jwt.token)
   })
 })
