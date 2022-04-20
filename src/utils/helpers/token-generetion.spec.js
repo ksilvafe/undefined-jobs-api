9680ab006b5edd6cf24381d964bf48aa8cdda04f
kevinferreiradev <ferreirakevin325@gmail.com>
2022-04-20 00:36:57 -0300

    move TokenGeneration to its own file

--- src/utils/helpers/token-generetion.spec.js
+++ src/utils/helpers/token-generetion.spec.js
@@ -1,21 +1,6 @@
+const TokenGenerator = require('./token-generation')
 const jwt = require('jsonwebtoken')
 const MissingParamError = require('../../utils/errors/missing-param-error')
-
-class TokenGenerator {
-  constructor (secret) {
-    this.secret = secret
-  }
-
-  async generate (id) {
-    if (!this.secret) {
-      throw new MissingParamError('secret')
-    }
-    if (!id) {
-      throw new MissingParamError('id')
-    }
-    return jwt.sign(id, this.secret)
-  }
-}
 
 const makeSut = () => {
   return new TokenGenerator('secret')
